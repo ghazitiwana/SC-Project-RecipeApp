7cdde15336e66ec67fe01dee9a1c00112cf707b5
ghazitiwana <35327403+ghazitiwana@users.noreply.github.com>
2020-04-12 15:43:46 +0500

    More ugliness

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -16,9 +16,7 @@ const formatCount = count => {
 const createIngredient = ingredient =>
   `
     <li class="recipe__item">
-        <svg class="recipe__icon">
-            <use href="img/icons.svg#icon-check"></use>
-        </svg>
+        
         <div class="recipe__count">${formatCount(ingredient.count)}</div>
         <div class="recipe__ingredient">
             <span class="recipe__unit">${ingredient.unit}</span>
@@ -38,16 +36,10 @@ export const renderRecipe = (recipe, isLiked) => {
       </figure>
       <div class="recipe__details">
           <div class="recipe__info">
-              <svg class="recipe__info-icon">
-                  <use href="img/icons.svg#icon-stopwatch"></use>
-              </svg>
-              <span class="recipe__info-data recipe__info-data--minutes">${recipe.time}</span>
+              <span class="recipe__info-data recipe__info-data--minutes">Time: ${recipe.time}</span>
               <span class="recipe__info-text"> minutes</span>
           </div>
           <div class="recipe__info">
-              <svg class="recipe__info-icon">
-                  <use href="img/icons.svg#icon-man"></use>
-              </svg>
               <span class="recipe__info-data recipe__info-data--people">${recipe.serving}</span>
               <span class="recipe__info-text"> servings</span>
 
@@ -80,9 +72,6 @@ export const renderRecipe = (recipe, isLiked) => {
           </ul>
 
           <button class="btn-small recipe__btn recipe__btn--add">
-              <svg class="search__icon">
-                  <use href="img/icons.svg#icon-shopping-cart"></use>
-              </svg>
               <span>Add to shopping list</span>
           </button>
       </div>
@@ -90,14 +79,10 @@ export const renderRecipe = (recipe, isLiked) => {
       <div class="recipe__directions">
           <h2 class="heading-2">How to cook it</h2>
           <p class="recipe__directions-text">
-              This recipe was carefully designed and tested by
-              <span class="recipe__by">${recipe.author}</span>. Please check out directions at their website.
+              Click here to get directions for this recipe
           </p>
           <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
               <span>Directions</span>
-              <svg class="search__icon">
-                  <use href="img/icons.svg#icon-triangle-right"></use>
-              </svg>
 
           </a>
       </div>
